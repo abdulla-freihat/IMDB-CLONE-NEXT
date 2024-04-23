08e3396744450ef43c144f3e927149ad73ad5efd
abdulla freihat <abdulla_freihat@hotmail.com>
2024-04-22 23:19:22 -0400

    fetch data from tmdb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,29 @@
 
-function HomePage() {
+import Results from "@/components/Results";
+const API_KEY = process.env.API_KEY;
+
+async function HomePage({ searchParams }) {
+  const genre = searchParams.genre ||'Trending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "TopRated" ? "/movie/top_rated" : "/trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  const data = await res.json()
+
+   if(!res.ok){
+
+     throw new Error('Faild to fetch!')
+   }
+
+    const results = data.results;
+    console.log(results);
+
   return (
-    <div>Home</div>
-  )
+
+       <Results results = {results} />
+  );
 }
 
-export default HomePage
+export default HomePage;
