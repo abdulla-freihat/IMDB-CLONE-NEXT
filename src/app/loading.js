e92c3b9dd9fed631d695a8498fa2093eee4e8ed0
abdulla freihat <abdulla_freihat@hotmail.com>
2024-04-23 02:35:37 -0400

    edit loadingeffect color

--- src/app/loading.js
+++ src/app/loading.js
@@ -4,8 +4,8 @@ import MoonLoader from "react-spinners/MoonLoader";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center mt-16 ">
-      <MoonLoader className="h-52" color="#f1f2f3" aria-label="Loading Spinner" />
+    <div className="flex justify-center mt-10 ">
+      <MoonLoader className="h-52" color='#f89c0c' aria-label="Loading Spinner" />
     </div>
   );
 };
